54542bac64d6fe7a4e1fac21ac29882c653f7b20
Adria Romero <adriaromero@me.com>
2018-11-04 23:30:02 -0500

    add ReadMe images

--- utils/delate_brackets.py
+++
@@ -1,9 +0,0 @@
-file_name = '/Users/AdriaRomeroLopez/Desktop/utils/method3_VGG16_scores_test.txt'
-with open(file_name, 'r') as my_file:
-    text = my_file.read()
-    text = text.replace("[", "")
-    text = text.replace("]", "")
-
-# If you wish to save the updates back into a cleaned up file
-with open(file_name, 'w') as my_file:
-    my_file.write(text)
